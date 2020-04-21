5656966c1d0c071a5ccac1ef30e396b4ff7203d3
namaragol <nagaraj.amaragol@cognizant.com>
2020-04-20 22:25:16 -0700

    commit 1

--- src/main/java/com/cts/Hello.java
+++ src/main/java/com/cts/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("It should work !!");
+        System.out.println("It should work this time!!");
     }
 }
